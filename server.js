f0a35dc1ae8dce0eda24d8b900af03a788bbbce9
krivetko <saperich@gmail.com>
2023-02-14 01:26:35 +0700

    Fixed Express-server script

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use('/', express.static([__dirname, 'static'].join('/')));
-app.use('/src', express.static([__dirname, 'src'].join('/')));
+app.use('/', express.static([__dirname, 'dist'].join('/')));
 
 app.listen(PORT, function () {
   console.log(`Sprint 1`);
